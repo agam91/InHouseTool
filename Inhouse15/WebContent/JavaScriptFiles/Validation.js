b45aa12421b6306712fc1b135fd23e1e20d78fe9
Agam <agam.mittal119@gmail.com>
2014-08-27 15:26:13 +0545

    Second Save

--- Inhouse15/WebContent/JavaScriptFiles/Validation.js
+++ Inhouse15/WebContent/JavaScriptFiles/Validation.js
@@ -6,6 +6,6 @@ function load(){
 function numberCheck(contactno){
 	
 	if(isNaN(contactno.value)||(contactno.value.length!=10)){
-		alert("Phone number "+contactno.value+" is not valid");
+		alert("Contact number "+contactno.value+" is not valid");
 	}
 }
