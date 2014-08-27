d6d1898ab878508485fd88db90a65b564294ee3e
agam91 <agam.mittal119@gmail.com>
2014-08-27 17:56:17 +0530

    Update Validation.js
    Yet Another Change

--- Inhouse15/WebContent/JavaScriptFiles/Validation.js
+++ Inhouse15/WebContent/JavaScriptFiles/Validation.js
@@ -6,6 +6,6 @@ function load(){
 function numberCheck(contactno){
 	
 	if(isNaN(contactno.value)||(contactno.value.length!=10)){
-		alert("Contact number "+contactno.value+" is not valid :(");
+		alert("Contact number "+contactno.value+" is not valid :( :(");
 	}
 }
