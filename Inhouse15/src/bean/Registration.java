6385d9ac9b767d6ef90a235ba4985aa71e29fee2
Agam <agam.mittal119@gmail.com>
2014-08-27 15:49:41 +0545

    Third save with bean class 'Registration'.

---
+++ Inhouse15/src/bean/Registration.java
@@ -0,0 +1,36 @@
+package bean;
+
+public class Registration {
+	
+	private String name;
+	private String pwd;
+	private String email;
+	private double contact;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPwd() {
+		return pwd;
+	}
+	public void setPwd(String pwd) {
+		this.pwd = pwd;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public double getContact() {
+		return contact;
+	}
+	public void setContact(double contact) {
+		this.contact = contact;
+	}
+	
+	
+}
